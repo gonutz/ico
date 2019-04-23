8b438765cc719f29814e43800f1e6139dc789846
Lars <lars.scheme@gmail.com>
2019-04-23 11:59:13 +0200

    Initial commit
    This has a fixed output: an icon with 9 different sizes (16x16 to
    256x256) generated from one input image by resizing it bilinearly. The
    output icon is formatted internally with PNGs instead of BMPs.
    
    In the future we probably want to support custom sizes in the icon,
    separate images for each size, support for BMP or PNG encoding.

---
+++ cmd/ico/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "golang.org/x/image/bmp"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/gonutz/ico"
+)
+
+func usage() {
+	fmt.Println(`usage: ico IMAGE-FILE [OUTPUT-FILE]
+  provide an input image (BMP, JPEG, PNG or GIF)
+  optionally provide an output file (ICO)
+  if no output file is given the input path will be used with extension .ico`)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stderr, "not enough arguments")
+		usage()
+		return
+	}
+	if len(os.Args) > 3 {
+		fmt.Fprintln(os.Stderr, "too many arguments")
+		usage()
+		return
+	}
+	in := os.Args[1]
+	var out string
+	if len(os.Args) == 3 {
+		out = os.Args[2]
+	} else {
+		ext := filepath.Ext(in)
+		out = strings.TrimSuffix(in, ext) + ".ico"
+	}
+
+	f, err := os.Open(in)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot open input file:", err)
+		return
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot read input image: ", err)
+		return
+	}
+
+	icon := ico.FromImage(img)
+	err = ioutil.WriteFile(out, icon, 0666)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot write output file:", err)
+		return
+	}
+}
---
+++ ico.go
@@ -0,0 +1,53 @@
+package ico
+
+import (
+	"bytes"
+	"encoding/binary"
+	"image"
+	"image/png"
+
+	"github.com/nfnt/resize"
+)
+
+func FromImage(img image.Image) []byte {
+	var buf bytes.Buffer
+	buf.Write([]byte{
+		0, 0, // reserved
+		1, 0, // 1 for .ico, 2 for .cur
+		9, 0, // number of images in this icon
+	})
+	sizes := []int{16, 24, 32, 48, 64, 96, 128, 192, 256}
+	var images [][]byte
+	for _, size := range sizes {
+		images = append(images, resizedImageData(img, size))
+	}
+	fileOffset := 6 + len(sizes)*16 // header size, first image starts here
+	for i, size := range sizes {
+		buf.Write([]byte{
+			// width,height; 256 wraps to 0 which is interpreted as 256
+			byte(size), byte(size),
+			0,    // no color palette
+			0,    // reserved
+			1, 0, // color planes, unused
+			32, 0, // bits per pixel
+		})
+		// append data length
+		binary.Write(&buf, binary.LittleEndian, uint32(len(images[i])))
+		// append data start offset in bytes from file start
+		binary.Write(&buf, binary.LittleEndian, uint32(fileOffset))
+		fileOffset += len(images[i]) // next image starts after this one
+	}
+	for _, img := range images {
+		buf.Write(img)
+	}
+	return buf.Bytes()
+}
+
+func resizedImageData(img image.Image, size int) []byte {
+	if img.Bounds().Dx() != size || img.Bounds().Dy() != size {
+		img = resize.Resize(uint(size), uint(size), img, resize.Bilinear)
+	}
+	var buf bytes.Buffer
+	png.Encode(&buf, img)
+	return buf.Bytes()
+}
